c3831091c620961749a6e09d77176e4c4e6b68dd
Shubhanshi Shaurya <shubhanshishaurya2006@gmail.com>
2026-03-10 21:59:36 +0530

    Initial commit:static to-do list

---
+++ frontend/script.js
@@ -0,0 +1,45 @@
+//JAVASCRIPT
+
+
+const inputBox=document.getElementById("input-box");
+const listContainer=document.getElementById("list-container");
+
+
+function addTask(){
+    if(inputBox.value===''){
+        document.getElementById("error").textContent="*The list must contain something."
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7"
+        li.appendChild(span);
+    }
+    inputBox.value="";
+    saveData();
+}
+
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
+
+
